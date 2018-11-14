c8774eec6ae1c33e12200a8330729be581501aac
Matteo Muscas <thatman.j@gmail.com>
2018-11-14 14:54:03 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
-end
+    new_array = []
+    array.each do |element|
+    new_array << element**2
+    end
+    new_array
+  end
+
+##def square_array(array)
+##  new_array = []
+##  array.each {|element| new_array << element**2}
+##  new_array
+##end
