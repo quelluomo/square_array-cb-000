70994f2ed1f1fb23586b0341d10c8d33eff73b88
Matteo Muscas <thatman.j@gmail.com>
2018-11-14 14:49:58 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
+## def square_array(array)
+##    new_array = []
+##    array.each do |element|
+##    new_array << element**2
+##    end
+##    new_array
+## end
+
 def square_array(array)
-    new_array = []
-    array.each do |element|
-    new_array << element**2
-    end
-    new_array
+  new_array = []
+  array.each {|element| new_array << element**2}
+  new_array
 end
